720a8f84efb39066d23b6cd48b4d5593a26eb2d3
destinybarbery <destiny.barbery@gmail.com>
2021-10-12 09:56:12 -0400

    my bad

--- script.js
+++ script.js
@@ -8,50 +8,34 @@ function addR() {
 
     alert("Clicked Add Row")
     
-    // check if columns = 0
-    if(numCols == 0)
-        numCols++
+    let mainGrid = document.getElementById("main-grid");
+    let newRow = document.createElement("tr");
 
-    // insert row
-    var row = grid.insertRow() 
-    grid.appendChild(row);
-
-    // insert cells
-    // loop - columns
-    for (var i = 0; i < numCols; i++) {
-        // Add Cell 
-        var newCell = row.insertCell(i);  
-        newCell.appendChild(numRows);
-        newCell.onclick = function() {
-            return changeColor(this.parentElement.rowIndex, this.cellIndex+1)
-        }
-    }   
-    numRows++;
+    for(let i = 0; i < amountofColumns; i++) {
+        let cell = document.createElement("td");
+        initializeCell(cell)
+        cell.classList.add("uncolored");
+        newRow.appendChild(cell);
+    }
+    mainGrid.appendChild(newRow);
+    amountofRows++;
 
 }
 
-//Adds a column
+
 function addC() {
 
     alert("Clicked Add Col")
 
-    // check if rows = 0
-    if(numRows == 0) {
-        return addR()
-    }
-    
-    // insert cells
-    // outer loop - columns
-    for (var i = numCols++; i < numCols; i++) {
-        // inner loop - rows
-        for (var j=0; j < numRows; j++) {
-            // Add Cell 
-            var newCell = grid.rows[j].insertCell(i);  
-            newCell.appendChild(numCols);
-            newCell.onclick = function() {
-                return changeColor(this.parentElement.rowIndex, this.cellIndex+1)
-            }
-        }
-    }
+      let mainGrid = document.getElementById("main-grid");
+    let allRows = document.querySelectorAll("tr");
+    let rowCounter = 0;
 
+    for(let i = 0; i < amountofRows; i++) {
+        let cell = document.createElement("td");
+        initializeCell(cell)
+        allRows[rowCounter].appendChild(cell);
+        rowCounter++;
+    }
+    amountofColumns++;
 }
